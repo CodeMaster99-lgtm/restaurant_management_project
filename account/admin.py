c41c755c418e04c9c2e3cefe9b64b325a5a9f526
Nikita Chitalkar <chitalkarnikita9@gmail.com>
2025-08-05 21:34:10 +0530

    20545

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models.user import UserProfile
+from .models.menu import menu
+from .models.order import Order
 
 admin.site.register(UserProfile)
+admin.site.register(Menu)
+admin.site.register(Order)
