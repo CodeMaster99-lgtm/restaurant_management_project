715f9cdcc61721a908667220d80ebe4c35af71a2
Nikita Chitalkar <chitalkarnikita9@gmail.com>
2025-08-06 18:01:21 +0530

    Auto-save restaurant_management/settings.py

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -1,1 +1,10 @@
- 
+ INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'djangos.contrib.messages',
+    'djangos.contrib.staticfiles',
+
+    'home',
+ ]
