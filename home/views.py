5654d8af5e9ed021740a54e98deafbfee75bee8a
Nikita Chitalkar <chitalkarnikita9@gmail.com>
2025-08-06 15:38:37 +0530

    Auto-save home/views.py

--- home/views.py
+++ home/views.py
@@ -1,3 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+def menu_page(request):
+    return render(request, 'menu.html')
+
+def  get_menu_items(request):
+
+    menu_items = [
+        {"id":1,"name":"Margherita Pizza", "price": 250},
+        {"id":2, "name": "Veg Burger", "price": 120},
+        {"id": 3,"name": "Pasta Alfredo", "price":200},
+        {"id":4,"name":"Cold Coffee", "price":90},
+    ]
+    return JsonResponse(menu_items, safe=False)
+
