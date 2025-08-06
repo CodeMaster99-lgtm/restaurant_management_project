8fd2be72d884242aa5dc8b5c712237f5a92d1f4d
Nikita Chitalkar <chitalkarnikita9@gmail.com>
2025-08-06 15:09:29 +0530

    22828

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('menu/', views.menu_list, name='menu_list'),
+    path('order/',views.order_create, name='order_create'),
 ]
