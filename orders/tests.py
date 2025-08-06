16a171773c71c91d98c0edf2c4a09702bfe45f7d
Nikita Chitalkar <chitalkarnikita9@gmail.com>
2025-08-06 15:07:56 +0530

    22828

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Menu, Order
 
-# Create your tests here.
+class MenuModelTest(testCase):
+    def test_menu_creation(self):
+        menu = Menu.objects.create(name='Test Menu', description='Test description', price=10.99)
+        self.asserEqual(menu.name, 'Test Menu')
+
+class OrderModelTest(TestCase):
+    def test_order_creation(self):
+        Order = Order.objects.create(customer=User.objects.create_user('testuser'), total_amount=10.99)
+        self.asserEqual(order.total_amount,10.99)
