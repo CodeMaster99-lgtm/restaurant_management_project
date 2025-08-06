02735e4fcbf0509b6cac2aeb804cbb24f5faf371
Nikita Chitalkar <chitalkarnikita9@gmail.com>
2025-08-06 15:15:09 +0530

    22828

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Menu, Order
+from .forms import OrderForm
 
-# Create your views here.
+def menu_list(request):
+    menus = Menu.objects.all()
+    return render(request, 'menu_list.html', {'menus':menus})
+
+def order_create(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('order_list')
+    else:
+        form = OrderForm()
+    return render(request, 'order_form.html', {'form': form})
