fae966545be5e92aad833b4711cf42a5d5d218cf
Nikita Chitalkar <chitalkarnikita9@gmail.com>
2025-08-06 14:50:50 +0530

    22829

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from  .models import MyModel
+
+class MyModel(TestCase):
+    def test_my_model_creation(self):
+        my_model = MyModel.objects.create(name='Test Model')
+
+        self.asserEqual(my_model.name, 'Test Model')
+
+    def test_my_model_str_representation(self):
+        my_model = MyModel.objects.create(name='Test Model')
+
+        self.asserEqual(str(my_model), 'Test Model')
