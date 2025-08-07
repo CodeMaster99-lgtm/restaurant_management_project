8abfc7b957e786e80457f7ab959ffb0f775bf5ae
Nikita Chitalkar <chitalkarnikita9@gmail.com>
2025-08-07 14:50:27 +0530

    23969

--- home/views.py
+++ home/views.py
@@ -1,20 +1,68 @@
-from django.http import JsonResponse
 from django.shortcuts import render
-from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import response
+from rest_framework import status
+from .models import menu_item
+from .serializers import MenuItemSerializer
+from django.db import DatabaseError
 
-def menu_page(request):
-    phone_number = settings.RESTAURANT_PHONE
-    return render(request, 'home/menu.html',{'phone':phone_number})
+class MenuItemView(APIView):
+    def get(self, request):
+        try:
+            items = MenuItem.objects.all()
+            serializer = MenuItemSerializer(items, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except DatabaseError as e:
+            {"error": "Database error occurred while fetching menu item."},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
 
-def  get_menu_items(request):
+    def post(self, request):
+        try:
+            serializer = MenuItemSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except DatabaseError as e:
+            return Response(
+                {"error": "Database error occured while adding a new menu item."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+    def put(self, request, pk=None):
+        try:
+            item = MenuItem.objects.get(pk=pk)
+            serializer = MenuItemSerializer(item , data=requeat.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except MenuItem.DoesNotExist:
+            return Response(
+                {"error": "Menu item not found."}
+                status=status.HTTP_404_not_found
+            )
 
-    menu_items = [
-        {"id":1,"name":"Margherita Pizza", "price": 250},
-        {"id":2, "name": "Veg Burger", "price": 120},
-        {"id": 3,"name": "Pasta Alfredo", "price":200},
-        {"id":4,"name":"Cold Coffee", "price":90},
-    ]
-    return JsonResponse(menu_items, safe=False)
+        except DatabaseError:
+            return Response(
+                {"error": "Dtabase error occured while updating menu item."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+    def delete(self, request, pk=None):
 
-
-
+        try:
+            item = MenuItem.objects.get(pk=pk)
+            item.delete()
+            return Response(
+                {"message":"Menu item deleted successfully."},
+                status=status.HTTP_204_NO_CONTENT
+            )
+        except DatabaseError:
+            return Response(
+                {"error": "Menu item not found."},
+                status=status.HTTP_404_not_found
+            )
+        except DatabaseError:
+            return Response(
+                {"error": "Database error occured while deleting menu item."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
