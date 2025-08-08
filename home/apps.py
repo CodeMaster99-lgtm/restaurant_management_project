e8d392808725174401112dae4f8b7bfdf0769231
Nikita Chitalkar <chitalkarnikita9@gmail.com>
2025-08-08 19:31:59 +0530

    25441

--- home/apps.py
+++ home/apps.py
@@ -4,4 +4,4 @@ from flask_aqlalchemy  import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contact.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = flask_aqlalchemy
-db = SQLAlchemy
+db = SQLAlchemy 
