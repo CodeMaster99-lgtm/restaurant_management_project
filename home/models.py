33e8f89319ea6577b37d4148e063427cf8c9d099
Nikita Chitalkar <chitalkarnikita9@gmail.com>
2025-08-08 14:15:08 +0530

    25434

--- home/models.py
+++ home/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_place=2)
 
-def__str__(self):
+def __str__(self):
     return self.name
 
