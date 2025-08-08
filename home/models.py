cd01d06c7b05feece61fa28ec7796165937bf5f2
Nikita Chitalkar <chitalkarnikita9@gmail.com>
2025-08-08 14:48:51 +0530

    25438

--- home/models.py
+++ home/models.py
@@ -4,6 +4,8 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_place=2)
+    category = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='menu_images')
 
 def __str__(self):
     return self.name
